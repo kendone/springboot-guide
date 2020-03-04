1e98504840be54472f751377a10add2488868ef6
kendone <ken-done@outlook.com>
2020-03-04 22:08:58 +0800

    *

--- springboot-jdbc/src/main/resources/schema.sql
+++ springboot-jdbc/src/main/resources/schema.sql
@@ -1,7 +1,11 @@
-CREATE TABLE users
+CREATE TABLE `world`.`Untitled`
 (
-    id    int(11)      NOT NULL AUTO_INCREMENT,
-    name  varchar(100) NOT NULL,
-    email varchar(100) DEFAULT NULL,
-    PRIMARY KEY (id)
-);
+    `id`    int(11)                                                 NOT NULL AUTO_INCREMENT,
+    `name`  varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 4
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
