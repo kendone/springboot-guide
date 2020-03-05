5cd0ac95bbff6eff58a913398c6043bda067e0b7
kendone <ken-done@outlook.com>
2020-03-05 16:02:20 +0800

    *

--- springboot-jpa/src/main/java/com/kendo/domain/User.java
+++ springboot-jpa/src/main/java/com/kendo/domain/User.java
@@ -9,7 +9,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false, unique = true)
     private String email;
 
     public User() {
--- springboot-jpa/src/test/java/com/kendo/SpringbootJpaApplicationTests.java
+++ springboot-jpa/src/test/java/com/kendo/SpringbootJpaApplicationTests.java
@@ -1,13 +1,31 @@
 package com.kendo;
 
+import com.kendo.domain.User;
+import com.kendo.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpringbootJpaApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
-    void contextLoads() {
+    public void findAllUsers() {
+        List<User> users = userRepository.findAll();
+        assertTrue(users.size() > 0);
     }
 
+    @Test
+    public void findUserById() {
+        Optional<User> user = userRepository.findById(1);
+        assertNotNull(user.get());
+    }
 }
