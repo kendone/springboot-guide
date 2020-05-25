cf4c70a6e558eb4120b403ddddf4e298992f890f
kendone <ken-done@outlook.com>
2020-05-25 14:35:35 +0800

    **

--- springboot-mybatis/src/main/java/com/kendo/SpringBootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/kendo/SpringBootMybatisApplication.java
@@ -1,8 +1,11 @@
 package com.kendo;
 
+import com.kendo.plugin.SqlPrintInterceptor;
+import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author kendone
@@ -15,4 +18,13 @@ public class SpringBootMybatisApplication {
         SpringApplication.run(SpringBootMybatisApplication.class, args);
     }
 
+    /**
+     * 注册输出 sql 语句拦截器
+     *
+     * @return PrintSqlInterceptor
+     */
+    @Bean
+    public Interceptor printSqlInterceptor() {
+        return new SqlPrintInterceptor();
+    }
 }
