1eb7629d7622639698e4eae24bbf8a5797f1dd60
kendone <ken-done@outlook.com>
2020-05-26 23:27:35 +0800

    *

---
+++ springboot-webmvc-war/src/main/java/com/kendo/Person.java
@@ -0,0 +1,38 @@
+package com.kendo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author kendone
+ */
+@ConfigurationProperties(prefix = "person")
+public class Person {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- springboot-webmvc-war/src/main/java/com/kendo/SpringBootWebmvc4WarApplication.java
+++ springboot-webmvc-war/src/main/java/com/kendo/SpringBootWebmvc4WarApplication.java
@@ -1,22 +1,41 @@
 package com.kendo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author kendone
  */
 @SpringBootApplication
+@EnableConfigurationProperties(Person.class)
+@RestController
 public class SpringBootWebmvc4WarApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebmvc4WarApplication.class, args);
     }
 
+    @Autowired
+    private Person person;
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SpringBootWebmvc4WarApplication.class);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot";
+    }
+
+    @RequestMapping("/person")
+    public Person getUser() {
+        return person;
     }
 }
